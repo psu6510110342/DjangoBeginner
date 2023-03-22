071a951678acb34d9b3c7d81ef1805fdce6e1ae7
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-22 11:41:12 +0700

    use arr from py to html

--- Django/app_foods/views.py
+++ Django/app_foods/views.py
@@ -6,4 +6,4 @@ def foods(request):
     return render(request, 'app_foods/foods.html')
 
 def food(request, food_id):
-    return HttpResponse('food = ' + str(food_id))
+    return render(request, 'app_foods/food.html', context={'food_id':food_id})
