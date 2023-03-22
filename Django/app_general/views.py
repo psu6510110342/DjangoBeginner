ad9cea8ed11e08f5a262d6676cfa6e16757be70b
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-22 10:29:02 +0700

    create aboutPath

--- Django/app_general/views.py
+++ Django/app_general/views.py
@@ -3,4 +3,7 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('test')
+    return HttpResponse('<h1>home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>about</h1>')
