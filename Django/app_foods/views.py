b9d47f9b5d1a847da307525219d2cefd227175cc
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-24 12:35:56 +0700

    mock up database

--- Django/app_foods/views.py
+++ Django/app_foods/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+
+all_food = [
+    {'id': 1, 'title': 'Chocolate', 'price': 449, 'is_premium': True},
+    {'id': 2, 'title': 'Red', 'price': 349, 'is_premium': False},
+    {'id': 3, 'title': 'Bule', 'price': 349, 'is_premium': False}
+]
 
 # Create your views here.
+
+
 def foods(request):
-    return render(request, 'app_foods/foods.html')
+    context = {'foods': all_food}
+    return render(request, 'app_foods/foods.html', context)
+
 
 def food(request, food_id):
-    return render(request, 'app_foods/food.html', context={'food_id':food_id})
+    return render(request, 'app_foods/food.html', context={'food_id': food_id})
