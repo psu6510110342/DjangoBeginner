e09453635e9e6395c1d8e1456c23304ea7b2cf89
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-25 09:59:40 +0700

    add filter date

--- Django/server/settings.py
+++ Django/server/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'th'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Bangkok'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
