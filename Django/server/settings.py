67089eb827d698ea75e1957c51faa748f500fd7b
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-28 10:37:56 +0700

    connect MySQL

--- Django/server/settings.py
+++ Django/server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '322547',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
