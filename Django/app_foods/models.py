308878d524b48bd78e8a3b41660d53b2ad0c5001
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-03-28 10:48:19 +0700

    makemigrations

--- Django/app_foods/models.py
+++ Django/app_foods/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    title = models.CharField(max_length=60)
+    price = models.IntegerField()
+    is_premium = models.BooleanField(null=True)
+    promotion_end_at = models.DateField(null=True)
