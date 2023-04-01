f423163999b86a70f3c954411376ac93f3ed44e9
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-04-01 16:52:55 +0700

    Try to run data

--- Django/app_foods/views.py
+++ Django/app_foods/views.py
@@ -1,43 +1,21 @@
 from datetime import datetime
 from django.shortcuts import render
-
-all_food = [
-    {
-        'id': 1,
-        'title': 'Chocolate',
-        'price': 1449,
-        'is_premium': True,
-        'promosion_end_at': datetime(2022, 2, 28)
-    },
-    {
-        'id': 2,
-        'title': 'Red',
-        'price': 1349,
-        'is_premium': False,
-        'promosion_end_at': datetime(2022, 2, 15)
-    },
-    {
-        'id': 3,
-        'title': 'Bule',
-        'price': 1349,
-        'is_premium': False,
-        'promosion_end_at': datetime(2022, 2, 15)
-    }
-]
+from .models import Food
 
 # Create your views here.
 
 
 def foods(request):
-    context = {'foods': all_food}
+    all_foods = Food.objects.order_by('-is_premium')
+    context = {'foods': all_foods}
     return render(request, 'app_foods/foods.html', context)
 
 
 def food(request, food_id):
     one_food = None
-    try:
-        one_food = [f for f in all_food if f['id'] == food_id][0]
-    except IndexError:
-        print('empty')
+    #try:
+    #    one_food = [f for f in all_foods if f['id'] == food_id][0]
+    #except IndexError:
+    #    print('empty')
     context = {'food': one_food}
     return render(request, 'app_foods/food.html', context)
