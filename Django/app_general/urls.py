e14f16083c1fab4ae554bf2fc2f35f6ac3366a9b
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-04-01 17:49:06 +0700

    subscription page

--- Django/app_general/urls.py
+++ Django/app_general/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('subscription', views.subscription, name='subscription'),
+    path('sub_ty', views.sub_ty, name='sub_ty'),
 ]
--- Django/app_general/views.py
+++ Django/app_general/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from app_foods.models import Food
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,11 @@ def home(request):
 
 def about(request):
     return render(request, 'app_general/about.html')
+
+def subscription(request):
+    all_foods = Food.objects.order_by('-is_premium')
+    context = {'foods': all_foods}
+    return render(request, 'app_general/subscription_from.html', context )
+
+def sub_ty(request):
+    return render(request, 'app_general/sub_ty.html')
