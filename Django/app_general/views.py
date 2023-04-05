7a283b6cd05d974d86b71020907769aaa5f9eab6
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-04-05 11:18:56 +0700

    upgrade form num2

--- Django/app_general/views.py
+++ Django/app_general/views.py
@@ -13,10 +13,18 @@ def about(request):
     return render(request, 'app_general/about.html')
 
 def subscription(request):
-    if request.POST:
-        
-        return HttpResponseRedirect(reverse('sub_ty'))
-    form = SubForm()
+    if request.method == 'POST':
+        form = SubForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_sub = Subscription()
+            new_sub.name = data['name']
+            new_sub.email = data['email']
+            new_sub.save()
+            new_sub.food_set.set(data['food_set'])
+            return HttpResponseRedirect(reverse('sub_ty'))
+    else:
+        form = SubForm()
     context = {'form': form}
     return render(request, 'app_general/subscription_from.html', context )
 
