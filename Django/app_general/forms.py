abb8312588a62e136f4559a5a908c46702ab2a79
ภัทรกร นลินเบญจพรรณ <phattarakorn23228@gmail.com>
2023-04-05 11:01:29 +0700

    upgrade form and get method

--- Django/app_general/forms.py
+++ Django/app_general/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from app_foods.models import Food
+
+class FoodMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.title
+
 class SubForm(forms.Form):
     name = forms.CharField(max_length=60, required=True, label='name')
     email = forms.EmailField(max_length=60, required=True, label='email')
-    food_set = forms.ModelMultipleChoiceField(
+    food_set = FoodMultipleChoiceField(
         queryset=Food.objects.order_by('-is_premium'),
         required=True, 
         label='menu',
